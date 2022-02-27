0758020086bd22b473a6c53041f597b3dbb72ed4
artem <ki4igin1992@outlook.com>
2022-02-27 14:06:49 +0300

    Макрос для определения битовых полей для РВВ

--- test/code/main.c
+++ test/code/main.c
@@ -2,12 +2,38 @@
 #include <avr/sleep.h>
 #include <util/delay.h>
 
+#define DEF_REG_BITS(name, addr, bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0) \
+    volatile union                                                               \
+    {                                                                            \
+        uint8_t ALL;                                                             \
+        struct                                                                   \
+        {                                                                        \
+            uint8_t bit0 : 1;                                                    \
+            uint8_t bit1 : 1;                                                    \
+            uint8_t bit2 : 1;                                                    \
+            uint8_t bit3 : 1;                                                    \
+            uint8_t bit4 : 1;                                                    \
+            uint8_t bit5 : 1;                                                    \
+            uint8_t bit6 : 1;                                                    \
+            uint8_t bit7 : 1;                                                    \
+        };                                                                       \
+    } name __attribute__((io(addr)));
+
+DEF_REG_BITS(PORTA_BITS, &PORTA, P7, P6, P5, P4, P3, P2, P1, P0)
+
+// #define PORTA_BITS ((volatile struct PORTA_bits *)&PORTA)
+
 int main(void)
 {
     // Основной цикл
+
+    PORTA_BITS.P0 = 0;
+    PORTA_BITS.ALL = 5;
+
     while (1)
     {
-        
+        PORTA_BITS.P0 = 0;
+        PORTA_BITS.P0 = 1;
     }
 }
 /* End File */
